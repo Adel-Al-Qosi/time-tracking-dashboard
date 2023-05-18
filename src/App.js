24f1b63af295c013a4ae9a35102d81f2bfc9c96e
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-05-18 20:58:06 +0300

    add Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Body from './components/Body'
 
 function App() {
   return (
-    <Body />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Body />} />
+      </Routes>
+    </Router>
   );
 }
 
